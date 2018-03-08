33ecd096f4511abaaf6f5d0ff54802615096c6c2
Giovanni Basolu <giovanni.basolu@gmail.com>
2018-03-09 00:48:11 +0100

    First commit

---
+++ httpdocs/assets/js/language.js
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+var labelsMessagesIndex={
+	it: {
+		code:"Nome utente",
+		passwd:"Password"
+	},
+	en: {
+		code:"Username",
+		passwd:"Password"
+	}
+};
+
+var errMessagesLogin = {
+	it:{
+		code: {
+			required: "Il campo code &egrave; richiesto"
+		},
+		passwd: {
+			 required: "Il campo password &egrave; richiesto"
+		} 
+	},
+	en:{
+		code: {
+			required: "The field code is required"
+		},
+		passwd: {
+			required: "The field email is required"
+		} 
+	}
+};
+
+var ajaxErrorsLogin={
+it:{
+	generic:"Errore",
+	login:{
+		"107":"Username e/o password errati"
+	}
+},
+en:{
+	generic:"Error",
+	login:{
+		"107":"Username and/or password wrong"
+	}
+}
+
+};
+ 
+var labelsMessagesHome={
+	it: {
+		m_global_situation:"Situazione globale",
+		m_booking:"Booking",
+		m_summary:"Riassunto",
+		m_statistic_export:"Esporta dati statistiche",
+		label_welcome:"Benvenuto",
+		button_logout:"Esci",
+		global_situation_title:"Situazione globale",
+		booking_title:"Booking",
+		summary_title:"Riassunto",
+		date_table_title:"Data",
+		departures_table_title:"Partenze previste per il ",
+		arrivals_table_title:"Arrivi previsti per il ",
+		birthdays_table_title:"Compleanni per il giorno ",
+		presences_table_title:"Presenze nel giorno ",
+		home_index_title: "Applicazione di utilità GetsCamp",
+		export_statistics_title: "Esportazione statistiche",
+		global_situation_print:"Stampa",
+		"label-referral_year":"Anno"
+	},
+	en: {
+		m_global_situation:"Global Situation",
+		m_booking:"Booking",
+		m_summary:"Summary",
+		m_statistic_export:"Export statistic data",
+		label_welcome:"Welcome",
+		button_logout:"Logout",
+		global_situation_title:"Global situation",
+		booking_title:"Booking",
+		summary_title:"Summary",
+		date_table_title:"Date",
+		departures_table_title:"Departures in the day ",
+		arrivals_table_title:"Arrivals in the day ",
+		birthdays_table_title:"Birthdays in the day ",
+		presences_table_title:"Presences in the day ",
+		home_index_title: "Gestcamp Utility application",
+		export_statistics_title: "Statistics Export",
+		global_situation_print:"Print",
+		"label-referral_year":"Year"
+	}
+};
+
+
+
+
