de79094b7d4abb9685adcf0461f5de15880ec424
Giovanni Basolu <giovanni.basolu@gmail.com>
2018-03-09 01:02:42 +0100

    Users Widget

---
+++ httpdocs/assets/js/NetKamp/Widget/Users.js
@@ -0,0 +1,14 @@
+var NK_Widget_Users=function() {
+
+	
+	var _super = new NK_Widget();
+	var _self = $.extend(
+		true,
+		_super,
+		{
+			className: "NK_Widget_Users"
+		});
+	
+	return _self;
+};
+
